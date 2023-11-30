fde93715af2a8fd51a18e02901dd09f8b905c059
ParasyteGS <alexander.astorga@utec.edu.pe>
2023-11-30 11:37:46 -0500

    update pre final

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from bson import ObjectId
 from flask_bcrypt import Bcrypt
 from datetime import datetime
 from bson import DBRef
+import base64
     
 
 
@@ -22,6 +23,8 @@ db = client['Electrade']
 componentes_collection = db['componentes']
 servicios_collection = db['servicios']
 user_collection = db['users']
+components_collection = db['components']
+
 
 @app.route('/')
 def index():
@@ -134,11 +137,32 @@ def editar_perfil():
         document_type = user.get('document_type')
         if document_type == 'dni':
             document = user.get('dni')
+            company = None
         else:
             document = user.get('ruc')
             company = user.get('company')
+        
+        if request.method == 'POST':
+            new_name = request.form['new_name']
+            new_email = request.form['new_email']
+            new_celular = request.form['new_celular']
             
-        return render_template('editar_perfil.html', username=username, name=name, email=email, celular=celular, document_type=document_type, document=document,  company=company, )
+            update_data = {'name': new_name, 'email': new_email, 'celular': new_celular}
+            
+            if document_type == 'dni':
+                update_data['dni'] = request.form['new_dni']
+            else:
+                update_data['ruc'] = request.form['new_ruc']
+                update_data['company'] = request.form['new_company']
+            
+            user_collection.update_one(
+                {'username': session['username']},
+                {'$set': update_data}
+            )
+            
+            return redirect(url_for('editar_perfil'))
+                        
+        return render_template('editar_perfil.html', username=username, name=name, email=email, celular=celular, document_type=document_type, document=document, company=company )
 
 
 # Registro para vendedores
@@ -204,59 +228,105 @@ def seller_products():
 
     return redirect(url_for('login'))
 
-@app.route('/add_componente', methods=['POST', 'GET'])
-def add_product():
-    if request.method == 'POST':
-        name = request.form['nombre']
-        description = request.form['descripcion']
-        price = float(request.form['precio'])
-        stock = int(request.form['stock'])
-        
-        seller = session.get('user_id')
-        # Procesar la imagen
-        image_file = request.files['imagen']
-        image_data = image_file.read()
-        encoded_img = Binary(image_data, sub_type=128)
-        
-        # Almacenar en mongoDB
-        componente_doc = {
-            'name': name,
-            'description': description,
-            'price': price,
-            'stock': stock,
-            'image': encoded_img
-        }
-        
-        componentes_collection.insert_one(componente_doc)
-    return render_template('add_componente.html')
+@app.route('/agregar_componente', methods=['POST', 'GET'])
+def agregar_componente():
+    if 'logged_in' in session:
+        if request.method == 'POST':
+            nombre = request.form['nombre']
+            descripcion = request.form['descripcion']
+            precio = float(request.form['precio'])
+            stock = int(request.form['stock'])
+            condicion = request.form['condicion']  # Obtener el valor del select
 
-@app.route('/edit_componente/<componente_id>', methods=['POST', 'GET'])
-def edit_product(componente_id):
-    
-    componente = componentes_collection.find_one({'_id': ObjectId(componente_id)})
-    
-    if request.method == 'POST':
-        name = request.form['nombre']
-        description = request.form['descripcion']
-        price = float(request.form['precio'])
-        stock = int(request.form['stock'])
+            
+            # Manejo de la imagen
+            image = request.files['imagen']
+            image_data = image.read()
+            image_base64 = base64.b64encode(image_data).decode('utf-8')
+            
+            user = user_collection.find_one({'username': session['username']})
+            if user:
+                user_id = user['_id']
+                
+                user_ref = DBRef('users', user_id)
+                
+                component_data = {
+                    'nombre': nombre,
+                    'descripcion': descripcion,
+                    'precio': precio,
+                    'stock': stock,
+                    'condicion': condicion,
+                    'image': image_base64,
+                    'user_id': user_ref
+                }
+                
+                component_id = components_collection.insert_one(component_data).inserted_id
+            
+                user_collection.update_one({'_id': user_id}, {'$push': {'componentes': DBRef('components', component_id)}})
+            
+                return redirect(url_for('detalle_componente', componente_id=component_id))
+        return render_template('add_component.html')
+    return redirect(url_for('login'))
+@app.route('/detalle_componente/<componente_id>')
+def detalle_componente(componente_id):
+    if 'logged_in' in session:
         
+        componente = components_collection.find_one({'_id': ObjectId(componente_id)})
         
-        componentes_collection.update_one(
-            {'_id': ObjectId(componente_id)},
-            {'$set': {
-                'name': name,
-                'description': description,
-                'price': price,
-                'stock': stock
-            }}
-        )
+        if componente:
+            return render_template('detalle_componente.html', componente=componente)
         
-        return redirect(url_for('componentes'))
-        
-        
-    return render_template('edit_componente.html', componente=componente)
+        return redirect(url_for('index'))
+    return redirect(url_for('login'))        
 
+@app.route('/editar_componente/<componente_id>', methods=['POST', 'GET'])
+def editar_componente(component_id):
+    if 'username' in session:
+        componente = components_collection.find_one({'_id': ObjectId(component_id)})
+        
+        user = user_collection.find_one({'username': session['username']})
+        es_vendedor = user['role'] == 'Vendedor' if user else Falsuevoe
+        
+        if es_vendedor:
+            if requested.method == 'POST':
+                nuevo_nombre = request.form['nombre']
+                nueva_descripcion = request.form['descripcion']
+                nuevo_precio = float(request.form['precio'])
+                nuevo_stock = int(request.form['stock'])
+                
+                update_data = {
+                    '$set': {
+                        'nombre': nuevo_nombre,
+                        'descripcion': nueva_descripcion,
+                        'precio': nuevo_precio,
+                        'stock': nuevo_stock
+                    }
+                
+                }
+                
+                components_collection.update_one({'_id': ObjectId(component_id)}, update_data)
+                
+                return redirect(url_for('detalle_componente', componente_id=component_id))
+        return render_template('editar_componente.html', componente_id=componente_id)
+    return redirect(url_for('login'))
+                
+@app.route('/eliminar_componente/<componente_id>', methods=['POST', 'GET'])
+def eliminar_componente(componente_id):
+    if 'username' in session:
+        
+        user = user_collection.find_one({'username': session['username']})
+        es_vendedor = user['role'] == 'Vendedor' if user else False
+        
+        if es_vendedor:
+            
+            components_collection.delete_one({'_id': ObjectId(componente_id)})
+            
+            user_collection.update_many({},{'$pull': {'componentes': {'$id': ObjectId(componente_id)}}})
+            
+            return redirect(url_for('seller_products'))
+        
+        return redirect(url_for('index'))
+    return redirect(url_for('login'))
 
 
 
